ec9a177c5a81370f0d9779c63a27bad3d7bfde94
Joel Rytkönen <rytkonenjoel@gmail.com>
2024-04-15 11:27:57 +0300

    Calculate total cost

--- RetailCalculator/retail_calculator.py
+++ RetailCalculator/retail_calculator.py
@@ -7,3 +7,6 @@ print(f"Selling {num_of_goods} goods.")
 value_of_good = float(input("Input the value of one good: "))
 assert isinstance(value_of_good, float)
 print(f"Selling goods with price {value_of_good}.")
+
+total_price = num_of_goods * value_of_good
+print(f"Total price of transaction: {total_price}")
