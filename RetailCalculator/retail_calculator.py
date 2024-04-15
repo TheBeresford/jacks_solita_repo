951187b638ecc3a9c58e0b76efa233c38f1ce086
Joel Rytkönen <rytkonenjoel@gmail.com>
2024-04-15 11:25:46 +0300

    Add value input

--- RetailCalculator/retail_calculator.py
+++ RetailCalculator/retail_calculator.py
@@ -3,3 +3,7 @@ print("Retail calculator started.")
 num_of_goods = int(input("Input the number of goods: "))
 assert isinstance(num_of_goods, int)
 print(f"Selling {num_of_goods} goods.")
+
+value_of_good = float(input("Input the value of one good: "))
+assert isinstance(value_of_good, float)
+print(f"Selling goods with price {value_of_good}.")
