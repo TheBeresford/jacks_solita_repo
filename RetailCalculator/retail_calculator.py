454d38144d897b1268aed35da8c0e813a03ff668
Joel Rytkönen <rytkonenjoel@gmail.com>
2024-04-15 14:51:24 +0300

    Init calculator 2.0

--- RetailCalculator/retail_calculator.py
+++ RetailCalculator/retail_calculator.py
@@ -2,15 +2,32 @@ import math
 
 print("Retail calculator started.")
 
-num_of_goods = int(input("Input the number of goods: "))
-assert isinstance(num_of_goods, int)
+product_names_and_prices = {
+    "bike": 100,
+    "quadbike": 500,
+    "car": 15000,
+    "helicopter": 1000000,
+    "airplane": 3000000,
+    "kissa": 5000000,
+}
+
+while True:
+    name_of_good = input("Input the name of the good: ")
+    if name_of_good in product_names_and_prices.keys:
+        break
+    else:
+        print("Invalid product name.")
+print(f"Selling {name_of_good}.")
+
+while True:
+    num_of_goods = int(input("Input the number of goods: "))
+    if isinstance(num_of_goods, int):
+        break
+    else:
+        print("Invalid number of goods.")
 print(f"Selling {num_of_goods} goods.")
 
-value_of_good = float(input("Input the value of one good: "))
-assert isinstance(value_of_good, float)
-print(f"Selling goods with price {value_of_good}.")
-
-total_price = num_of_goods * value_of_good
+total_price = num_of_goods * name_of_good
 print(f"Total price of transaction: {round(total_price)}")
 
 
@@ -24,19 +41,19 @@ def requirements_for_next_discount(total_price, value_of_good, discount_rate_cei
 # Discounts
 if total_price < 1000:
     discount_percentage = 0
-    requirements_for_next_discount(total_price, value_of_good, 1000)
+    requirements_for_next_discount(total_price, name_of_good, 1000)
 elif total_price >= 1000 and total_price < 5000:
     discount_percentage = 0.03
-    requirements_for_next_discount(total_price, value_of_good, 5000)
+    requirements_for_next_discount(total_price, name_of_good, 5000)
 elif total_price >= 5000 and total_price < 7000:
     discount_percentage = 0.05
-    requirements_for_next_discount(total_price, value_of_good, 7000)
+    requirements_for_next_discount(total_price, name_of_good, 7000)
 elif total_price >= 7000 and total_price < 10000:
     discount_percentage = 0.07
-    requirements_for_next_discount(total_price, value_of_good, 10000)
+    requirements_for_next_discount(total_price, name_of_good, 10000)
 elif total_price >= 10000 and total_price < 50000:
     discount_percentage = 0.10
-    requirements_for_next_discount(total_price, value_of_good, 50000)
+    requirements_for_next_discount(total_price, name_of_good, 50000)
 else:
     discount_percentage = 0.15
 
@@ -63,7 +80,7 @@ print(f"Total price after tax: {round(price_after_tax, 2)}\n")
 kuitti = "-" * 16
 
 kuitti = kuitti + f"\nNumber of items...{num_of_goods}"
-kuitti = kuitti + f"\nValue of individual items...{value_of_good}"
+kuitti = kuitti + f"\nValue of individual items...{name_of_good}"
 kuitti = kuitti + f"\nTotal price.......{total_price}"
 kuitti = kuitti + f"\n\nDiscount...{discount_amount}"
 kuitti = kuitti + f"\nPrice after discount...{price_after_discount}"
